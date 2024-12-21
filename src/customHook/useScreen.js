ca156301bd7465aa86d093dc92e1b03f30fd4bd7
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2024-12-21 15:08:33 +0530

    feat: Added useScreen custom hook to check smaller screen

---
+++ src/customHook/useScreen.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const useScreen = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 640);
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+
+    return isSmallScreen;
+  }, []);
+};
+
+export default useScreen;
