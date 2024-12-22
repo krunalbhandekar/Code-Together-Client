d88c117f4fe6831294621ff2e284d280c803e725
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2024-12-22 15:37:29 +0530

    feat: Added tech stacks svg icons

---
+++ src/assets/icons/VercelIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const VercelIcon = () => {
+  return (
+    <svg
+      fill="#000000"
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-10 w-10"
+    >
+      <title>Vercel</title>
+      <path d="M24 22.525H0l12-21.05 12 21.05z" />
+    </svg>
+  );
+};
+
+export default VercelIcon;
